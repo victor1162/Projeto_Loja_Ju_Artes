08b5ee7f2ee41a87bfd79fa6ef91f63d9e726aee
Victor santos <victor_santos1162@hotmail.com>
2021-04-23 10:48:08 -0300

    Projeto loja ju artes

---
+++ js/script.js
@@ -0,0 +1,79 @@
+function abrirCatalogo1(){
+	let uls1 = document.getElementById('drop1')
+	let areaDropdown1 = document.querySelector('.catalogo-dropdown')
+	
+	if(areaDropdown1.style.height === '0px'){
+
+		areaDropdown1.style.height = '146px'
+			uls1.style.opacity = '1'
+			uls1.style.display = 'block'
+			areaDropdown1.style.transition = '1s'
+			areaDropdown1.style.background = '#FF6BB5'
+			uls1.style.color = ''
+
+	}else{
+		areaDropdown1.style.height = '0px'
+		uls1.style.opacity = '0'
+		uls1.style.display = 'none'
+	}
+}
+
+function abrirCatalogo2(){
+	let uls2 = document.getElementById('drop2')
+	let areaDropdown2 = document.getElementById('abrir2')
+
+	if(areaDropdown2.style.height === '0px'){
+
+		areaDropdown2.style.height = '146px'
+			uls2.style.opacity = '1'
+			uls2.style.display = 'block'
+			areaDropdown2.style.transition = '1s'
+			areaDropdown2.style.background = '#FF6BB5'
+			uls2.style.color = ''
+
+	}else{
+		areaDropdown2.style.height = '0px'
+		uls2.style.opacity = '0'
+		uls2.style.display = 'none'
+	}
+}
+
+function abrirCatalogo3(){
+	let uls3 = document.getElementById('drop3')
+	let areaDropdown3 = document.getElementById('abrir3')
+
+	if(areaDropdown3.style.height === '0px'){
+
+		areaDropdown3.style.height = '146px'
+			uls3.style.opacity = '1'
+			uls3.style.display = 'block'
+			areaDropdown3.style.transition = '1s'
+			areaDropdown3.style.background = '#FF6BB5'
+			uls3.style.color = ''
+
+	}else{
+		areaDropdown3.style.height = '0px'
+		uls3.style.opacity = '0'
+		uls3.style.display = 'none'
+	}
+}
+
+function abrirCatalogo4(){
+	let uls4 = document.getElementById('drop4')
+	let areaDropdown4 = document.getElementById('abrir4')
+
+	if(areaDropdown4.style.height === '0px'){
+
+		areaDropdown4.style.height = '146px'
+			uls4.style.opacity = '1'
+			uls4.style.display = 'block'
+			areaDropdown4.style.transition = '1s'
+			areaDropdown4.style.background = '#FF6BB5'
+			uls4.style.color = ''
+
+	}else{
+		areaDropdown4.style.height = '0px'
+		uls4.style.opacity = '0'
+		uls4.style.display = 'none'
+	}
+}
